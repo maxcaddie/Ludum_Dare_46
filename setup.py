0c0e18fdd9c607623d446700d995d70067e9d19e
Unknown <maxscaddie@gmail.com>
2020-04-18 11:00:57 +1000

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("TestPygame.py")]
+
+cx_Freeze.setup(
+    name="A bit Racey",
+    options={"build_exe": {
+        "packages":["pygame"],
+        "excludes": ['tcl', 'ttk', 'tkinter', 'Tkinter', 'scipy']}},
+    executables = executables
+
+    )
