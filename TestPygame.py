34885e2f1301eceecf7d3618ddc906bc4eefaa45
Unknown <maxscaddie@gmail.com>
2020-04-18 12:14:10 +1000

    Update git

--- TestPygame.py
+++ TestPygame.py
@@ -1,23 +1,36 @@
 import pygame
 
-pygame.init()
+SIZE = 1.2
+HEIGHT = int(SIZE*800)
+WIDTH = int(SIZE*600)
+white = [255, 255, 255]
 
-gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption('A bit Racey')
 
-clock = pygame.time.Clock()
-crashed = False
+def main():
+    pygame.init()
 
-while not crashed:
+    game_display = pygame.display.set_mode((HEIGHT, WIDTH))
+    pygame.display.set_caption('TBC')
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
+    game_display.fill(white)
 
-        print(event)
+    clock = pygame.time.Clock()
+    crashed = False
 
-    pygame.display.update()
-    clock.tick(60)
+    while not crashed:
 
-pygame.quit()
-quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+
+            print(event)
+
+        pygame.display.update()
+        clock.tick(60)
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    main()
